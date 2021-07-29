a4f3b5cfcd1bba9603eae3461d0c3cf077970b21
AnomDev <serbla.dev@gmail.com>
2021-07-29 21:19:13 +0200

    Project structure added

---
+++ app/src/main/java/com/anomdev/platziconf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.anomdev.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anomdev.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
